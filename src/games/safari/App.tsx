a0c1630b04692b4df92c4cf7aa6747b44dc6bf53
Eyvgeniy <antonm33@yandex.ru>
2021-03-25 22:14:13 +0200

    add two states

--- src/games/safari/App.tsx
+++ src/games/safari/App.tsx
@@ -4,28 +4,34 @@ import './App.css';
 
 const question = 'option';
 const answers = ['лодка', 'настройка', 'везти', 'этаж'];
+const rightAnswer = 2;
 
 const App = (): JSX.Element => {
-  const [state, setState] = React.useState('waitingAnswer');
+  const [gameState, setGameState] = React.useState('waitingAnswer');
   const [position, setPosition] = React.useState(100);
+  const [clickedAnswer, setClickedAnswer] = React.useState(null);
 
-  const buttonClass = cn('button-answer', { rightAnswer: state === 'rightAnswer' });
+  const buttonClass = cn('button-answer', { rightAnswer: gameState === 'rightAnswer' });
+  const questionClass = cn('safari-question', { questionFall: gameState === 'waitingAnswer' });
 
   const handleAnswer = (number: number) => (): void => {
-    if (number === 2) setState('rightAnswer');
+    if (number === 2) setGameState('rightAnswer');
   };
 
   React.useEffect(() => {
-    let timeout: any;
-    if (state === 'waitingAnswer') {
-      timeout = setTimeout(() => setPosition((currentPosition) => currentPosition - 10), 5000);
+    let timeout: number;
+    if (gameState === 'rightAnswer') {
+      setPosition((currentPosition) => currentPosition - 10);
+      timeout = window.setTimeout(() => {
+        setGameState('waitingAnswer');
+      }, 2000);
     } else {
-      timeout = setTimeout(() => setPosition((currentPosition) => currentPosition - 10), 5000);
+      timeout = window.setTimeout(() => setGameState('rightAnswer'), 5000);
     }
     return () => {
       clearTimeout(timeout);
     };
-  }, [position]);
+  }, [gameState]);
 
   return (
     <section className="savanna-main-container" style={{ backgroundPositionY: `${position}%` }}>
@@ -40,13 +46,24 @@ const App = (): JSX.Element => {
         </div>
       </div>
       <div className="safari-game">
-        <div className="safari-question">{question}</div>
+        <div className={questionClass}>{question}</div>
         <div className="safari-answers">
-          {answers.map((answer, i) => (
-            <div className={buttonClass} key={i + 1} onClick={handleAnswer(i + 1)}>{`${
-              i + 1
-            }) ${answer}`}</div>
-          ))}
+          {answers.map((answer, i) => {
+            if (gameState === 'waitingAnswer') {
+              return (
+                <div className={buttonClass} key={i + 1} onClick={handleAnswer(i + 1)}>{`${
+                  i + 1
+                }) ${answer}`}</div>
+              );
+            }
+            return (
+              <div
+                className={i + 1 === rightAnswer ? buttonClass : 'button-answer'}
+                key={i + 1}
+                onClick={handleAnswer(i + 1)}
+              >{`${i + 1}) ${answer}`}</div>
+            );
+          })}
         </div>
       </div>
     </section>
