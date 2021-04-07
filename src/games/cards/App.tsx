d9899471062240a37136be1c0134393638dddb3a
Natallia22 <zhuravleva.977@yandex.ru>
2021-04-07 21:59:45 +0300

    fix: fix route game

--- src/games/cards/App.tsx
+++ src/games/cards/App.tsx
@@ -1,33 +1,25 @@
-import * as React from 'react';
-import { useSelector } from 'react-redux';
-import Start from './components/Start';
-import CardGame from './components/CardGame';
-import './App.css';
+import * as React from "react";
+import { useSelector } from "react-redux";
+import Start from "./components/Start";
+import CardGame from "./components/CardGame";
+import "./App.css";
 
 const App = (): JSX.Element => {
-  const [gameState, setGameState] = React.useState('start');
-  //const [position, setPosition] = React.useState(100);
-  
+  const [gameState, setGameState] = React.useState("start");
+
   return (
-    <section className="main-container" >
+    <section className="main-container">
       <div className="close-button">
         <span
           className="close-button__text"
           onClick={() => {
-            setGameState('start');
+            setGameState("start");
           }}
         >
           &times;
         </span>
       </div>
-      {gameState === 'start' ? (
-        <Start setState={setGameState} />
-      ) : (
-        <CardGame
-          /*gameState={gameState}
-          setGameState={setGameState}*/
-        />
-      )}
+      {gameState === "start" ? <Start setState={setGameState} /> : <CardGame />}
     </section>
   );
 };
