4af47e5dbac1a4aa08a8f518454228a2bcd8e28c
Eyvgeniy <antonm33@yandex.ru>
2021-04-12 09:56:08 +0200

    added delete and toHard methods for words

--- src/api/wordsApi.ts
+++ src/api/wordsApi.ts
@@ -30,6 +30,36 @@ const setWordToLearned = (user: any, wordId: string): void => {
   fetchSetWordsState(user, wordId, wordData);
 };
 
+const updateWordToLearned = (user: any, wordId: string): void => {
+  const wordData = { difficulty: 'normal' };
+  fetchChangeWordsState(user, wordId, wordData);
+};
+
+const setWordToDeleted = (user: any, wordId: string): void => {
+  const wordData = { difficulty: 'easy' };
+  fetchSetWordsState(user, wordId, wordData);
+};
+
+const updateWordToDeleted = (user: any, wordId: string): void => {
+  const wordData = { difficulty: 'easy' };
+  fetchChangeWordsState(user, wordId, wordData);
+};
+
+const setWordToHard = (user: any, wordId: string): void => {
+  const wordData = { difficulty: 'hard' };
+  fetchSetWordsState(user, wordId, wordData);
+};
+
+const updateWordToHard = (user: any, wordId: string): void => {
+  const wordData = { difficulty: 'hard' };
+  fetchChangeWordsState(user, wordId, wordData);
+};
+
 export default {
   setWordToLearned,
+  updateWordToLearned,
+  setWordToDeleted,
+  updateWordToDeleted,
+  setWordToHard,
+  updateWordToHard,
 };
