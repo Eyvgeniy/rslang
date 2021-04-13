5f861a37b24c4076d699435842add416b220b356
Eyvgeniy <antonm33@yandex.ru>
2021-04-13 17:29:45 +0200

    feature: added auth for dictionary link

--- src/components/BookNav/BookNav.tsx
+++ src/components/BookNav/BookNav.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './BookNav.css';
 
-const BookNav = (): JSX.Element => (
-  <div className='book-nav'>
-    <a href='#book'>Учебник</a>
-    <a href='#dictionary'>Словарь</a>
-  </div>
-);
+const BookNav = (): JSX.Element => {
+  const user = useSelector((state: any) => state.user.currentUser);
+  return (
+    <div className='book-nav'>
+      <a href='#book'>Учебник</a>
+      {user && <a href='#dictionary'>Словарь</a>}
+    </div>
+  );
+};
 
 export default BookNav;
