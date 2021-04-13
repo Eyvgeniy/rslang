da7e074d718456c4011fdde05bca31e665c79e5e
Eyvgeniy <antonm33@yandex.ru>
2021-04-13 17:27:11 +0200

    feature: add routes for user words

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,23 @@ const getWords = (page = 0, group = 0): string =>
   [host, `words?page=${page}&group=${group}`].join('/');
 const getWordById = (id: string): string => [host, 'word', id].join('/');
 
+const getLearnedWords = (id: string, page = 0, group = 0): string =>
+  [
+    host,
+    `users/${id}/aggregatedWords?page=${page}&group=${group}&wordsPerPage=20&filter={"$or":[{"userWord.difficulty":"normal"},{"userWord.difficulty":"hard"}]}`,
+  ].join('/');
+
+const getUserWords = (id: string, page = 0, group = 0): string =>
+  [
+    host,
+    `users/${id}/aggregatedWords?page=${page}&group=${group}&wordsPerPage=20&filter={"userWord":{"$exists": true}}`,
+  ].join('/');
+
+const getAllUserWords = (id: string): string => [host, `users/${id}/words`].join('/');
+
+const setWordState = (userId: string, wordId: string) =>
+  [host, `users/${userId}/words/${wordId}`].join('/');
+
 const signIn = (): string => [host, 'signin'].join('/');
 const createUser = (): string => [host, 'users'].join('/');
 const updateUser = (id: string): string => [host, 'users', id].join('/');
@@ -17,6 +34,10 @@ const getUserPhotoUrl = (fileName: string): string =>
 export default {
   getWords,
   getWordById,
+  getLearnedWords,
+  getUserWords,
+  getAllUserWords,
+  setWordState,
   createUser,
   updateUser,
   deleteUser,
