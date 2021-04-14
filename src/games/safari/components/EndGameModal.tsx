ea90ce2d2eff034d41b8ce5809caed21b1c3c67a
Eyvgeniy <antonm33@yandex.ru>
2021-04-14 22:05:07 +0200

    feature: add end game modal

---
+++ src/games/safari/components/EndGameModal.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const EndGameModal = ({
+  closeModal,
+  show,
+}: {
+  closeModal: Function;
+  showModal: Function;
+  show: boolean;
+}): JSX.Element => {
+  return (
+    <>
+      <Modal show={show} onHide={closeModal}>
+        <Modal.Body>Вы потратили все жизни и проиграли</Modal.Body>
+        <Modal.Footer>
+          <Button variant='primary' onClick={closeModal}>
+            Играть еще!
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default EndGameModal;
