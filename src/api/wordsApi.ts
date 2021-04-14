04667fd9c4672fd20652c7c24c88dbfcdbff3a54
Eyvgeniy <antonm33@yandex.ru>
2021-04-14 18:35:12 +0200

    feature: add api to delete user word

--- src/api/wordsApi.ts
+++ src/api/wordsApi.ts
@@ -2,60 +2,73 @@ import axios from 'axios';
 import routes from '../routes';
 
 const fetchSetWordsState = async (user: any, wordId: string, wordData: any) => {
-  try {
-    await axios.post(routes.setWordState(user.id, wordId), wordData, {
+  return axios
+    .post(routes.setWordState(user.id, wordId), wordData, {
       headers: {
         Authorization: `Bearer ${user.token}`,
       },
+    })
+    .catch((err) => {
+      throw new Error(err);
     });
-  } catch (err) {
-    throw new Error(err);
-  }
 };
 
-const fetchChangeWordsState = async (user: any, wordId: string, wordData: any) => {
-  try {
-    await axios.put(routes.setWordState(user.id, wordId), wordData, {
+const fetchChangeWordsState = (user: any, wordId: string, wordData: any) => {
+  return axios
+    .put(routes.setWordState(user.id, wordId), wordData, {
       headers: {
         Authorization: `Bearer ${user.token}`,
       },
+    })
+    .catch((err) => {
+      throw new Error(err);
     });
-  } catch (err) {
-    throw new Error(err);
-  }
 };
 
-const setWordToLearned = (user: any, wordId: string, stats: any): void => {
+const fetchDeleteUserWord = (user: any, wordId: string) => {
+  return axios
+    .delete(routes.setWordState(user.id, wordId), {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+};
+
+const setWordToLearned = (user: any, wordId: string, stats: any): Promise<unknown> => {
   const wordData = { difficulty: 'normal', optional: { stats } };
-  fetchSetWordsState(user, wordId, wordData);
+  return fetchSetWordsState(user, wordId, wordData);
 };
 
-const updateWordToLearned = (user: any, wordId: string, stats: any): void => {
+const updateWordToLearned = (user: any, wordId: string, stats: any): Promise<unknown> => {
   const wordData = { difficulty: 'normal', optional: { stats } };
-  fetchChangeWordsState(user, wordId, wordData);
+  return fetchChangeWordsState(user, wordId, wordData);
 };
 
-const setWordToDeleted = (user: any) => (wordId: string): void => {
+const setWordToDeleted = (user: any) => (wordId: string): Promise<unknown> => {
   const wordData = { difficulty: 'easy' };
-  fetchSetWordsState(user, wordId, wordData);
+  return fetchSetWordsState(user, wordId, wordData);
 };
 
-const updateWordToDeleted = (user: any) => (wordId: string): void => {
+const updateWordToDeleted = (user: any) => (wordId: string): Promise<unknown> => {
   const wordData = { difficulty: 'easy' };
-  fetchChangeWordsState(user, wordId, wordData);
+  return fetchChangeWordsState(user, wordId, wordData);
 };
 
-const setWordToHard = (user: any) => (wordId: string): void => {
+const setWordToHard = (user: any) => (wordId: string): Promise<unknown> => {
   const wordData = { difficulty: 'hard' };
-  fetchSetWordsState(user, wordId, wordData);
+  return fetchSetWordsState(user, wordId, wordData);
 };
 
-const updateWordToHard = (user: any) => (wordId: string, stats: any): void => {
+const updateWordToHard = (user: any) => (wordId: string, stats: any): Promise<unknown> => {
   const wordData = { ...stats, difficulty: 'hard' };
-  fetchChangeWordsState(user, wordId, wordData);
+  return fetchChangeWordsState(user, wordId, wordData);
 };
 
 export default {
+  fetchDeleteUserWord,
   setWordToLearned,
   updateWordToLearned,
   setWordToDeleted,
